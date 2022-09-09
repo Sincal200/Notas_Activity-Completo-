9121b41767e7c0b05466c051a117bd86026cc547
Sincal <sincaljulio200@gmail.com>
2022-09-09 10:34:24 -0600

    Se añadieron mas restaurantes

--- app/src/main/java/com/example/restaurante/RestauranteFragment.java
+++ app/src/main/java/com/example/restaurante/RestauranteFragment.java
@@ -74,6 +74,9 @@ public class RestauranteFragment extends Fragment {
             restauranteList = new ArrayList<>();
             restauranteList.add(new Restaurante("Pizzeria Carlos","https://th.bing.com/th/id/R.ad93a6a7158d0a67c8e874a946b16faf?rik=lBaReMFlvQa6dg&pid=ImgRaw&r=0",4.0f,"Madrid, España"));
             restauranteList.add(new Restaurante("Hamburgueseria rápida","https://th.bing.com/th/id/R.600596d992025c657fdc67d8e9af6661?rik=YI%2bC%2fo98p%2ftCyQ&pid=ImgRaw&r=0",3.0f,"Mexico"));
+            restauranteList.add(new Restaurante("Venta de Mariscos","https://d1ralsognjng37.cloudfront.net/5bdc8613-9273-4f40-addc-418ada8a8cc5.jpeg",1f,"Guatemala"));
+            restauranteList.add(new Restaurante("Restaurante Pedro","https://fullhdpictures.com/wp-content/uploads/2016/11/Restaurant-Photos-HD.jpg",2f,"Honduras"));
+
 
 
 
